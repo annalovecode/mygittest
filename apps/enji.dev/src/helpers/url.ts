d49b7ea305fd54873109f6e7461aea8016b1eae3
annaz <46110477+annalovecode@users.noreply.github.com>
2023-08-03 04:30:44 -0700

    Update url.ts

--- apps/enji.dev/src/helpers/url.ts
+++ apps/enji.dev/src/helpers/url.ts
@@ -1,7 +1,7 @@
 export const getBaseUrl = () => {
   switch (process.env.NEXT_PUBLIC_VERCEL_ENV) {
     case 'production':
-      return 'https://mygittest-annalovecode.vercel.app/';
+      return 'https://anna-zhao.online/';
     case 'preview':
       return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
     default:
